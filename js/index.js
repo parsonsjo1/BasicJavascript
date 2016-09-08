aaa0840621de300ae7784c0ed0b41cb125373656
parsonsjo1 <jopa.d.parsons@gmail.com>
2016-09-08 14:29:54 -0600

    Finished and testing

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function setFontFamily(font) {
+	document.getElementById('display_text').style.fontFamily = font;
+}
+
+function setColor(color) {
+	document.getElementById('display_text').style.color = color;
+}
+
+function setText(text) {
+	document.getElementById('display_text').innerHTML = text;
+}
